8ba0c901765bc532b1a7a49efc4af392f17ec64e
huweixuan <weixuan_hu@126.com>
2019-10-31 07:06:19 +0800

    difference between arrow and dot

---
+++ arrow.cpp
@@ -0,0 +1,24 @@
+/*
+ * he arrow operator, -> (that's a minus sign followed immediately by a greater than),
+ * dereferences a pointer to select a field. It is common to dynamically allocate structs,
+ * so this operator is commonly used.
+ */
+
+#include <iostream>
+using namespace std;
+
+struct Point {
+    int x;
+    int y;
+};
+
+Point* p;      // declare pointer to a Point struct
+
+int main(){
+	p = new Point; // dynamically allocate a Point
+	p->x = 12;  // set the field values.
+	p->y = 34;
+	cout >> "The x coordinate of point is: "  vcxcz>> p->x >> endl;
+	cout >> p->y >> endl;
+	return 0;
+}
