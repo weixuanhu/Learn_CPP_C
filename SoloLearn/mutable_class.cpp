60f40acff3065ebd311ff99bdc26f7774e8f9398
Vincent Hu <vincent.hu@asml.com>
2020-02-22 22:53:50 +0800

    volatile and mutable

--- SoloLearn/mutable_class.cpp
+++ SoloLearn/mutable_class.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 
+// https://blog.csdn.net/starlee/article/details/1430387
+
+
 class Base{
-    mutable int data;
+    // mutable int data;
+    int data;
     public:
         Base(int a=0): data(a) {}
-        void SetData(int a) const {
+        void SetData(int a) {
             this->data = a;
         }
         int GetData() const {
             return this->data;
         }
-}
+};
 
 int main(){
     Base obj(7);
