a5b22796a0ab860f30596b7265167b392834adea
Vincent Hu <vincent.hu@asml.com>
2020-03-27 15:37:28 +0800

    static and shared library

---
+++ MISC/static_library/example1/hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/* 
+ *  g++ -static -o static_lib.exe hello_world.cpp
+ *  g++ -o shared_lib.exe hello_world.cpp
+ */
+
+int main(){
+    cout << "hello c++" << endl;
+}
---
+++ MISC/static_library/example2/add_test.cpp
@@ -0,0 +1,9 @@
+#include "add.hpp"
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number1 = 90;
+    int number2 = 15;
+    cout << "number1 and number2 sum is: " << add(number1, number2) << endl;
+}
