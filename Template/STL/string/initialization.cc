aee6bcc9ec9c4da2addbe5572ed2572e8fabf37b
Vincent Hu <vincent.hu@asml.com>
2020-06-23 08:27:33 +0800

    practice variadic

---
+++ Template/STL/string/initialization.cc
@@ -0,0 +1,9 @@
+#include <string.h>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    const char *pname = "huweixuan";
+    string arrayname(pname);
+    return 0;
+}
